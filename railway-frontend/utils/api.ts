fe4982f66db90ad59ef1ec32e3ccaf8b288d58cc
shivakumarhazari <119376087+Shiva-code-code@users.noreply.github.com>
2025-04-12 03:24:07 +0530

    Update api.ts

--- railway-frontend/utils/api.ts
+++ railway-frontend/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '🔗 https://9254-103-52-38-4.ngrok-free.app', 
+  baseURL: 'https://9254-103-52-38-4.ngrok-free.app', 
 });
 
 API.interceptors.request.use((req) => {
