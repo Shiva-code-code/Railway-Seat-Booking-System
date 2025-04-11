68279fd616f6ae52f31788c185cdf1216e24f437
shivakumarhazari <119376087+Shiva-code-code@users.noreply.github.com>
2025-04-11 19:44:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Required for Render PostgreSQL SSL
-  },
+  connectionString: process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false } // Required for Render PostgreSQL SSL
+    : false,
 });
 
 module.exports = pool;
-
