486a84a560dec39e54ef9a3ba8dfe0a688787074
shivakumarhazari <119376087+Shiva-code-code@users.noreply.github.com>
2025-04-11 18:09:26 +0530

    Update api.ts with backend api

--- railway-frontend/utils/api.ts
+++ railway-frontend/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://railway-seat-booking-system.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
@@ -13,3 +13,4 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
+
