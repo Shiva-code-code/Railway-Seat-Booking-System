3120da0ef2faf9b867460ff7b1c09fe35d0570ad
shivakumarhazari <119376087+Shiva-code-code@users.noreply.github.com>
2025-04-22 21:43:05 +0530

    Update api.ts

--- railway-frontend/utils/api.ts
+++ railway-frontend/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://5306-103-52-38-4.ngrok-free.app', 
+  baseURL: 'https://railway-seat-booking-system-1.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
